c34374a98b7e6f2cd502dfa7db02f031a2233ed9
frisca23 <frisca.putri23@students.utdi.ac.id>
2024-07-01 18:32:41 +0700

    Add files via upload

---
+++ Bentuk1Mod.java
@@ -0,0 +1,14 @@
+public class Bentuk1Mod
+{
+  public static void main(String[]args)
+  {
+    int a = 5;
+    int b = 1;
+    while (b <= a)
+    {
+      System.out.print('*');
+      System.out.println();
+      b++;
+    }
+  }
+}
---
+++ Modif5.java
@@ -0,0 +1,32 @@
+public class Modif5
+{
+  public static void main(String[]args)
+  {
+    int x = 5;
+    int i = 1;
+    while (i <= x)
+    {
+      int j = 4;
+      while (j >= 1);
+      {
+        System.out.print(' ');
+        j--;
+      }
+      int k = 1;
+      while (k <= i)
+      {
+        System.out.print(k);
+        k++;
+      }
+      int l = 1;
+      while (l <= i -1);
+      {
+        System.out.print(i);
+        l++;
+      }
+        System.out.println();
+        i++;
+      }
+    }
+}
+
